b97a5de3acdcf332d0ebf5af1c043d4b59a52187
Marcus Hammarberg <marcusoft.net@gmail.com>
2014-02-12 23:16:45 +0700

    Initalized application with config

--- app.js
+++ app.js
@@ -2,3 +2,26 @@
 // :| - meh
 // :) - happy
 // :D - above expectation
+// set up
+var express	= require("express");
+var app = express();
+var mongoose = require("mongoose");
+var config = require('./config')();
+
+// config
+mongoose.connect(config.mongoUrl);
+
+app.configure(function () {
+	app.use(express.static(__dirname + "/public"));
+	app.use(express.logger("dev"));
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(express.methodOverride());
+});
+
+// load routes
+//require('./app/routes')(app);
+
+// start it up
+app.listen(config.port);
+console.log("RateItJs (in '" + config.mode + "' mode) listening on port" + config.port);
